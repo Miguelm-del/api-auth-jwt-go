b747f6493b09902c84577144f11feef90b071332
Miguel Márcio <mmms2@aluno.ifal.edu.br>
2023-02-14 22:31:09 -0300

    Create dto

--- internal/dto/dto.go
+++ internal/dto/dto.go
@@ -4,3 +4,18 @@ type CreateProductInput struct {
 	Name  string  `json:"name"`
 	Price float64 `json:"price"`
 }
+
+type CreateUserInput struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type GetJWTInput struct {
+	Email    string
+	Password string
+}
+
+type GetJWTOutput struct {
+	AccessToken string `json:"access_token"`
+}
