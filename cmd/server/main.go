7c2dd7c4c3eb580aae785e0319dfcfffa264db28
Miguel Márcio <mmms2@aluno.ifal.edu.br>
2023-02-14 22:33:43 -0300

    Update main and add documentation

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,18 +2,34 @@ package main
 
 import (
 	"github.com/Miguelm-del/api-auth-jwt-go/configs"
+	_ "github.com/Miguelm-del/api-auth-jwt-go/docs"
 	"github.com/Miguelm-del/api-auth-jwt-go/internal/entity"
 	"github.com/Miguelm-del/api-auth-jwt-go/internal/infra/database"
 	"github.com/Miguelm-del/api-auth-jwt-go/internal/infra/webserver/handlers"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/jwtauth"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"net/http"
 )
 
+// @title Go Basic API
+// @version 1.0
+// @description Product API with authentication
+// @termsOfService http://swagger.io/terms/
+//
+// @contact.name Miguel Márcio
+// @contact.email miguelmarcio.dev@gmail.com
+//
+// @host localhost:8000
+// @BasePath /
+// @securityDefinitions.apiKey ApiKeyAuth
+// @in header
+// @name Authorization
 func main() {
-	_, err := configs.Load(".")
+	conf, err := configs.Load(".")
 	if err != nil {
 		panic(err)
 	}
@@ -25,9 +41,41 @@ func main() {
 	productDB := database.NewProduct(db)
 	productHandler := handlers.NewProductHandler(productDB)
 
+	userDB := database.NewUser(db)
+	userHandler := handlers.NewUserHandler(userDB)
+
 	r := chi.NewRouter()
+
+	// Middlewares
 	r.Use(middleware.Logger)
-	r.Post("/products", productHandler.CreateProduct)
+	r.Use(middleware.Recoverer)
+	// r.Use(LogRequest)
+	r.Use(middleware.WithValue("jwt", conf.TokenAuth))
+	r.Use(middleware.WithValue("JwtExpiresIn", conf.JWTExpiresIn))
+
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(conf.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/", productHandler.GetProducts)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
+
+	r.Post("/users", userHandler.Create)
+	r.Post("/users/generate_token", userHandler.GetJWT)
+
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8000/docs/doc.json")))
 
 	http.ListenAndServe(":8000", r)
 }
+
+// Middleware example
+//
+//func LogRequest(next http.Handler) http.Handler {
+//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//		log.Printf("Request: %s %s", r.Method, r.URL.Path)
+//		next.ServeHTTP(w, r)
+//	})
+//}
